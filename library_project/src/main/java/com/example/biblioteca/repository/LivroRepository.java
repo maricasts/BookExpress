462f6155d99dd758e6875f004353aa80552751cd
ʚଓㅤ  ოᥲɾі     ⩊ <mariaimaculada7773@gmail.com>
2025-10-29 22:27:10 +0000

    Versão atual atualiza no replit

--- library_project/src/main/java/com/example/biblioteca/repository/LivroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.biblioteca.repository;
-
-import com.example.biblioteca.model.Livro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface LivroRepository extends JpaRepository<Livro, Integer> {
-    List<Livro> findByQuantidade_disponivelGreaterThan(int q);
-}
